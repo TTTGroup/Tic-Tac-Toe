c57991ceb5def85e3dca26e0c95a8927ae8cb1a9
mibre2 <mibre2@morgan.edu>
2020-11-30 22:53:49 -0500

    commit

--- app/src/main/java/com/codepath/mibre2/tic_tac_toe/MainActivity.java
+++ app/src/main/java/com/codepath/mibre2/tic_tac_toe/MainActivity.java
@@ -8,44 +8,108 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    // 2d 3x3 array of buttons representing TTT Grid
     private Button[][] buttons = new Button[3][3];
-
-    // player 1 set first as default
     private boolean player1Turn = true;
-
     private int roundCount;
-
+    
     private int player1Points;
     private int player2Points;
 
-    private TextView tvPlayer1;
-    private TextView tvPlayer2;
-
-
+    private TextView textViewPlayer1;
+    private TextView textViewPlayer2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textViewPlayer1 = findViewById(R.id.text_view_p1);
+        textViewPlayer2 = findViewById(R.id.text_view_p2);
 
-        tvPlayer1 = findViewById(R.id.tvPlayer1);
-        tvPlayer2 = findViewById(R.id.tvPlayer2);
-
-        // Assign button arr to buttons using a nested loop
+        // Nested loop for grid
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                String btnId = "btn_" + i + j;
-                int resID = getResources().getIdentifier(btnId, "id", getPackageName());
+                String buttonID = "button_" + i + j;
+                int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
                 buttons[i][j] = findViewById(resID);
                 buttons[i][j].setOnClickListener(this);
             }
         }
+        Button buttonReset = findViewById(R.id.button_reset);
+        buttonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
     }
 
     @Override
     public void onClick(View v) {
+        if (!((Button) v).getText().toString().equals("")) {
+            return;
+        }
+        if (player1Turn) {
+            ((Button) v).setText("X");
+        } else {
+            ((Button) v).setText("O");
+        }
+        roundCount++;
+        if (checkForWin()) {
+            if (player1Turn) {
+                player1Wins();
+            } else {
+                player2Wins();
+            }
+        } else if (roundCount == 9) {
+            draw();
+        } else {
+            player1Turn = !player1Turn;
+        }
+    }
 
+    private boolean checkForWin() {
+        String[][] field = new String[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                field[i][j] = buttons[i][j].getText().toString();
+            }
+        }
+        for (int i = 0; i < 3; i++) {
+            if (field[i][0].equals(field[i][1])
+                    && field[i][0].equals(field[i][2])
+                    && !field[i][0].equals("")) {
+                return true;
+            }
+        }
+        for (int i = 0; i < 3; i++) {
+            if (field[0][i].equals(field[1][i])
+                    && field[0][i].equals(field[2][i])
+                    && !field[0][i].equals("")) {
+                return true;
+            }
+        }
+        if (field[0][0].equals(field[1][1])
+                && field[0][0].equals(field[2][2])
+                && !field[0][0].equals("")) {
+            return true;
+        }
+        if (field[0][2].equals(field[1][1])
+                && field[0][2].equals(field[2][0])
+                && !field[0][2].equals("")) {
+            return true;
+        }
+        // If nobody wins
+        return false;
+    }
+
+    // TODO: NEEDS COMPLETION
+    private void player1Wins() {
+    }
+
+    // TODO: NEEDS COMPLETION
+    private void player2Wins() {
+    }
+
+    // TODO: NEEDS COMPLETION
+    private void draw() {
     }
 }
